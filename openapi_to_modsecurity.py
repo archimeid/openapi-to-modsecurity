feee5677aef4fe4c930ff2c8bbeba3503354e79c
Archimeid <68751170+archimeid@users.noreply.github.com>
2021-11-11 02:06:44 +0100

    Load JSON and YAML in an OpenAPI object (dict)

--- openapi_to_modsecurity.py
+++ openapi_to_modsecurity.py
@@ -1,19 +1,28 @@
 import argparse
+from yaml import load, dump
+from yaml.cyaml import CLoader
+import json
+import pprint
 
 parser = argparse.ArgumentParser(description='Convert an OpenAPI file to ModSecurity rules.')
 parser.add_argument('openapi_file', type=str, help='Path to OpenAPI file')
 parser.add_argument('modsecurity_file', type=str, help='Path to ModSecurity rules file')
 
 args = parser.parse_args()
-openapi_file = args.openapi_file.split('/')[-1]
+openapi_filename = args.openapi_file.split('/')[-1]
 
-if '.' not in openapi_file:
+if '.' not in openapi_filename:
     raise NameError
 
-ext = openapi_file.split('.')[-1]
+ext = openapi_filename.split('.')[-1]
 
 if ext in ['yml', 'yaml']:
-    print('Loading a YAML file')
-
+    print('Loading a YAML file - ', openapi_filename)
+    file = open(args.openapi_file, 'r')
+    file_content = file.read()
+    openapi = load(file_content, Loader=CLoader)
+    
 if ext in ['json']:
-    print('Loading a JSON file')
+    print('Loading a JSON file - ', openapi_filename)
+    file = open(args.openapi_file, 'r')
+    openapi = json.load(file)
