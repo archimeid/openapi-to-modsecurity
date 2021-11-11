fc22273b48c4af8f88f2d8cd1b6630ec5384e71f
Archimeid <68751170+archimeid@users.noreply.github.com>
2021-11-11 01:49:49 +0100

    Detecting file ext on OpenAPI file

--- openapi_to_modsecurity.py
+++ openapi_to_modsecurity.py
@@ -1,7 +1,19 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Convert an OpenAPI file to ModSecurity rules.')
-parser.add_argument('openapi_file', type=str, help='OpenAPI file path')
-parser.add_argument('modsecurity_file', type=str, help='ModSecurity file path')
+parser.add_argument('openapi_file', type=str, help='Path to OpenAPI file')
+parser.add_argument('modsecurity_file', type=str, help='Path to ModSecurity rules file')
 
 args = parser.parse_args()
+openapi_file = args.openapi_file.split('/')[-1]
+
+if '.' not in openapi_file:
+    raise NameError
+
+ext = openapi_file.split('.')[-1]
+
+if ext in ['yml', 'yaml']:
+    print('Loading a YAML file')
+
+if ext in ['json']:
+    print('Loading a JSON file')
